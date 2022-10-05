5e1387b44badd997b87de6f0056d06c3e21d7b4b
jaduplansti <84189727+jaduplansti@users.noreply.github.com>
2022-10-05 16:42:35 +0800

    Add files via upload

--- VirtualCpu.c
+++ VirtualCpu.c
@@ -2,14 +2,19 @@
 #include <stdlib.h>
 
 FILE *src;
-int OpCodes[6] = {
+int Memory[255];
+
+int OpCodes[8] = {
     0x00, //Exit
     0x05, //Cmp
     0x09, //Mov
     0x01, //NOP
     0x07, //Jmp
     0x08, //SysCall
+    0x03, //Put 
+    0x02, //Pull
 };
+
 int Register_Code[3] = {
      0x20,
      0x28,
@@ -26,6 +31,7 @@ struct Flag {
   int rf; //Result Flag
   int of; //Overflow Flag
   int nf; // Null Flag
+  int ip; //Instruction Flag
 } Cur_Flag;
 
 struct Vm_Status {
@@ -34,6 +40,8 @@ struct Vm_Status {
     int DoNothing;
     int SysInterrupt;
     int Move;
+    int IsPut;
+    int IsPull;
 } Cur_Stat;
 
 struct Usage_Info {
@@ -57,11 +65,15 @@ int OpCode_Do(int Opcode)
         case 0x07:
         Cur_Stat.Jumping = 1;
         break;
+
+	case 0x09:
+	Cur_Stat.Move = 1;
+	break;
     }
 }
 
 int Operand_Do(int Operand1,int Operand2)
-{
+{	
     if(Cur_Stat.Comparing == 1)
     {
         if(Operand1 == Operand2)
@@ -71,6 +83,7 @@ int Operand_Do(int Operand1,int Operand2)
             return 0;
         }
     }
+
     if(Cur_Stat.Comparing == 1)
     {
         if(Cur_UsageInf.IsUsingSrc == 1)
@@ -81,4 +94,12 @@ int Operand_Do(int Operand1,int Operand2)
             }
         }
     }
+    
+    if(Cur_Stat.Move == 1)
+    {
+	Operand1 = Operand2;
+	Cur_Stat.Move = 0;
+    }
+
 }
+
